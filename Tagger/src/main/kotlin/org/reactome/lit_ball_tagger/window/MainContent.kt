3c2eda7f2ddda3debad557ba655bcf9939332598
Ralf Stephan <ralf@ark.in-berlin.de>
2023-04-28 17:51:23 +0200

    rough list layout

--- Tagger/src/main/kotlin/org/reactome/lit_ball_tagger/window/MainContent.kt
+++ Tagger/src/main/kotlin/org/reactome/lit_ball_tagger/window/MainContent.kt
@@ -2,31 +2,29 @@
 
 package org.reactome.lit_ball_tagger.window
 
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.*
 import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.reactome.lit_ball_tagger.common.Paper
+import org.reactome.lit_ball_tagger.common.Tag
 
 val MARGIN_SCROLLBAR: Dp = 0.dp
 @Suppress("FunctionName")
@@ -64,10 +62,11 @@ private fun ListContent(
 
         LazyColumn(state = listState) {
             items(items) { item ->
-                Item(
+                CardWithTextIconAndRadiobutton(
                     item = item,
                     onClicked = { onItemClicked(item.id) },
-                    onDeleteClicked = { onItemDeleteClicked(item.id) }
+                    onDeleteClicked = { onItemDeleteClicked(item.id) },
+                    onOptionSelected = {},
                 )
 
                 Divider()
@@ -82,6 +81,84 @@ private fun ListContent(
 }
 
 @Composable
+fun CardWithTextIconAndRadiobutton(
+    item: Paper,
+    onClicked: () -> Unit,
+    onOptionSelected: (Int) -> Unit,
+    onDeleteClicked: () -> Unit,
+) {
+    val cardTitle = item.details.title
+    val radioButtonOptions = Tag.values().map { it.name }
+    Card(
+        elevation = 4.dp,
+        backgroundColor = Color.White,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            IconButton(onClick = onDeleteClicked) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete Item",
+                    tint = Color.Gray,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = cardTitle,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.weight(1F).align(Alignment.CenterVertically),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            RadioButtonOptions(
+                radioButtonOptions,
+                item.tag.ordinal,
+                onOptionSelected
+            )
+        }
+    }
+}
+
+@Composable
+fun RadioButtonOptions(
+    options: List<String>,
+    defaultSelectedOptionIndex: Int,
+    onOptionSelected: (Int) -> Unit
+) {
+    var selectedOptionIndex by remember { mutableStateOf(defaultSelectedOptionIndex) }
+    options.forEachIndexed { index, option ->
+        Row(
+            modifier = Modifier
+                .padding(vertical = 4.dp, horizontal = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                modifier = Modifier.size(8.dp),
+                selected = index == selectedOptionIndex,
+                onClick = {
+                    selectedOptionIndex = index
+                    onOptionSelected(selectedOptionIndex)
+                }
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Text(
+                text = option,
+                fontSize = 8.sp,
+                color = Color.Black
+            )
+        }
+    }
+}
+
+/*
+@Composable
 private fun Item(
     item: Paper,
     onClicked: () -> Unit,
@@ -92,10 +169,18 @@ private fun Item(
 
         Text(
             text = AnnotatedString(item.details.title),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
             modifier = Modifier.weight(1F).align(Alignment.CenterVertically),
-            maxLines = 1,
+            maxLines = 3,
             overflow = TextOverflow.Ellipsis
         )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            TagsRadioButtons(item.tag)
+        }
 
         Spacer(modifier = Modifier.width(8.dp))
 
@@ -109,3 +194,36 @@ private fun Item(
         Spacer(modifier = Modifier.width(MARGIN_SCROLLBAR))
     }
 }
+
+@Composable
+fun TagsRadioButtons(tag: Tag) {
+    val radioOptions = Tag.values().map { it.name }
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(tag.name) }
+    Column {
+        radioOptions.forEach { text ->
+            Row(
+                Modifier
+                    .fillMaxWidth()
+//                    .selectableGroup()
+                    .selectable(
+                        selected = (text == selectedOption),
+                        onClick = {
+                            onOptionSelected(text)
+                        }
+                    )
+                    .padding(horizontal = 6.dp)
+            ) {
+                RadioButton(
+                    selected = (text == selectedOption),
+                    onClick = { onOptionSelected(text) }
+                )
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.body1.merge(),
+                    modifier = Modifier.padding(start = 10.dp)
+                )
+            }
+        }
+    }
+}
+*/
