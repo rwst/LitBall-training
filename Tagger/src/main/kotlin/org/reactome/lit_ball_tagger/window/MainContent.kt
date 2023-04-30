e87eb3bc583f3c7ca38134097f888041f2e96714
Ralf Stephan <ralf@ark.in-berlin.de>
2023-04-30 19:58:57 +0200

    make key scrolling work, after focus

--- Tagger/src/main/kotlin/org/reactome/lit_ball_tagger/window/MainContent.kt
+++ Tagger/src/main/kotlin/org/reactome/lit_ball_tagger/window/MainContent.kt
@@ -2,15 +2,13 @@
 
 package org.reactome.lit_ball_tagger.window
 
-import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.*
-import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -22,10 +20,7 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.KeyEvent
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
@@ -35,7 +30,6 @@ import kotlinx.coroutines.runBlocking
 import org.reactome.lit_ball_tagger.common.Paper
 import org.reactome.lit_ball_tagger.common.Tag
 
-val MARGIN_SCROLLBAR: Dp = 0.dp
 @Suppress("FunctionName")
 @Composable
 internal fun MainContent(
@@ -50,74 +44,83 @@ internal fun MainContent(
             onRailItemClicked = onRailItemClicked,
         )
 
-        Box(Modifier.weight(1F)) {
-            ListContent(
-                items = items,
-                onItemClicked = onItemClicked,
-                onItemDeleteClicked = onItemDeleteClicked
-            )
-        }
+        ListContent(
+            items = items,
+            onItemClicked = onItemClicked,
+            onItemDeleteClicked = onItemDeleteClicked
+        )
     }
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-private fun ListContent(
+fun ListContent(
     items: List<Paper>,
     onItemClicked: (id: Int) -> Unit,
     onItemDeleteClicked: (id: Int) -> Unit,
-) {
-    Box {
-        val listState = rememberLazyListState()
+    ) {
+    val focusRequester = remember { FocusRequester() }
+    val lazyListState = rememberLazyListState()
 
-        // Handle keyboard events to scroll the list
-        val scrollState = remember { ScrollState(0) }
-        val onKeyDownSuspend: suspend (KeyEvent) -> Boolean = {
-            when (it.key) {
-                Key.DirectionUp -> {
-                    scrollState.scrollTo(scrollState.value - 1)
-                    true
-                }
-                Key.DirectionDown -> {
-                    scrollState.scrollTo(scrollState.value + 1)
-                    true
-                }
-                else -> false
+    val onKeyDownSuspend: suspend (KeyEvent) -> Boolean = {
+        println(it.key)
+        when (it.type) {
+            KeyEventType.KeyUp -> {
+                false
             }
-        }
-        val onKeyDown: (KeyEvent) -> Boolean = {
-            runBlocking { onKeyDownSuspend(it) }
-        }
-
-        Box(modifier = Modifier.fillMaxSize()) {
-            // Listen for keyboard events
-            val focusRequester = remember { FocusRequester() }
-            DisposableEffect(Unit) {
-                focusRequester.requestFocus()
-                onDispose { }
-            }
-            Box(modifier = Modifier.focusRequester(focusRequester).fillMaxSize()) {
-                // Display the list
-                LazyColumn(state = listState,
-                    modifier = Modifier.fillMaxSize().onPreviewKeyEvent(onKeyDown)
-                ) {
-                    items(items) { item ->
-                        CardWithTextIconAndRadiobutton(
-                            item = item,
-                            onClicked = { onItemClicked(item.id) },
-                            onDeleteClicked = { onItemDeleteClicked(item.id) },
-                            onOptionSelected = {},
-                        )
-
-                        Divider()
+            else -> {
+                val topItem = lazyListState.firstVisibleItemIndex
+                val topOffset = lazyListState.firstVisibleItemScrollOffset
+                when (it.key) {
+                    Key.DirectionUp -> {
+                        if (topOffset > 0)
+                            lazyListState.scrollToItem(topItem)
+                        else if (topItem > 0)
+                            lazyListState.scrollToItem(topItem - 1)
+                        if (topOffset > 0)
+                            lazyListState.scrollToItem(topItem)
+                        else if (topItem > 0)
+                            lazyListState.scrollToItem(topItem - 1)
+                        true
                     }
+                    Key.DirectionDown -> {
+                        lazyListState.scrollToItem(topItem + 1)
+                        true
+                    }
+                    else -> false
                 }
             }
         }
+    }
+    val onKeyDown: (KeyEvent) -> Boolean = {
+        runBlocking { onKeyDownSuspend(it) }
+    }
 
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .focusRequester(focusRequester)
+            .clickable { focusRequester.requestFocus() }
+            .onPreviewKeyEvent(onKeyDown)
+    ) {
+        LazyColumn(
+            Modifier.fillMaxSize().padding(end = 12.dp),
+            lazyListState
+        ) {
+            items(items) { item ->
+                CardWithTextIconAndRadiobutton(
+                    item = item,
+                    onClicked = { onItemClicked(item.id) },
+                    onDeleteClicked = { onItemDeleteClicked(item.id) },
+                    onOptionSelected = {},
+                )
+                Divider()
+            }
+        }
         VerticalScrollbar(
             modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
-            adapter = rememberScrollbarAdapter(scrollState = listState)
+            adapter = rememberScrollbarAdapter(
+                scrollState = lazyListState
+            )
         )
     }
 }
@@ -198,3 +201,4 @@ fun RadioButtonOptions(
         }
     }
 }
+
