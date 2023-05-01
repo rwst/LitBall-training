4ddb43d5f791a90c96857c4308f9b7ca40a64050
Ralf Stephan <ralf@ark.in-berlin.de>
2023-05-01 13:07:31 +0200

    cleanup

--- Tagger/src/main/kotlin/org/reactome/lit_ball_tagger/window/MainContent.kt
+++ Tagger/src/main/kotlin/org/reactome/lit_ball_tagger/window/MainContent.kt
@@ -201,4 +201,3 @@ fun RadioButtonOptions(
         }
     }
 }
-
--- helper/src/main/kotlin/Logger.kt
+++
@@ -1,9 +0,0 @@
-import io.github.oshai.KotlinLogging
-
-object Log {
-    private val logger = KotlinLogging.logger {}
-    fun e(e: Exception) { logger.error(e.message) }
-    fun i(e: Exception) { logger.info(e.message) }
-    fun i(tag: String, e: Exception) { logger.info(tag + ": " + e.message) }
-    fun i(tag: String, s: String) { logger.info("$tag: $s") }
-}
