3cd2587aacfbc69a82bc28b5d4a9b0c534bc6605
Ralf Stephan <ralf@ark.in-berlin.de>
2023-06-03 11:13:06 +0200

    clickable list items (scrolling still buggy)

---
+++ Tagger/src/main/kotlin/org/reactome/lit_ball_tagger/common/dialog/ItemClickedDialog.kt
@@ -0,0 +1,68 @@
+package org.reactome.lit_ball_tagger.common.dialog
+
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.reactome.lit_ball_tagger.common.CurrentPaperList
+
+@OptIn(ExperimentalMaterialApi::class)
+@Suppress("FunctionName")
+@Composable
+internal fun ItemClickedDialog(id: Int, onDoneClicked: () -> Unit) {
+    val lazyListState = rememberLazyListState()
+    // TODO: Fix Scrolling
+    AlertDialog(
+        modifier = Modifier
+            .fillMaxSize(fraction = 0.8f)
+            .padding(12.dp),
+        title = {
+            Text(text = "Paper# $id")
+        },
+        text = {
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Row (
+                ) {
+                    LazyColumn(
+                        Modifier.fillMaxSize().padding(end = 12.dp).size(100.dp),
+                        lazyListState
+                    ) {
+                        item {
+                            Text(
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillMaxWidth(),
+                                text = CurrentPaperList.pretty(id),
+                            )
+                        }
+                    }
+                }
+                VerticalScrollbar(
+                    modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+                    adapter = rememberScrollbarAdapter(
+                        scrollState = lazyListState
+                    )
+                )
+            }
+        },
+        onDismissRequest = {},
+        confirmButton = {
+            TextButton(
+                onClick = { onDoneClicked() },
+            ) {
+                Text("Back")
+            }
+        },
+    )
+}
