28d9dc21a703e71231da003571adcb5d5d7a6670
Ralf Stephan <ralf@ark.in-berlin.de>
2023-07-17 19:04:23 +0200

    refactoring

--- Tagger/src/main/kotlin/org/reactome/lit_ball_tagger/common/EnrichedItems.kt
+++ Tagger/src/main/kotlin/org/reactome/lit_ball_tagger/common/EnrichedItems.kt
@@ -6,35 +6,41 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import java.io.File
 
+/**
+ * The EnrichedItems class provides functionality to initialize and return a map of DOI to value.
+ * It uses a JSON file to load data and update the map.
+ */
 object EnrichedItems {
     var initialized = false
         private set
-    private val map = mutableMapOf<String, Int>()
+    private val doiToValueMap = mutableMapOf<String, Int>()
     private val json = Json { ignoreUnknownKeys = true }
 
-    fun init() {
-        @Serializable
-        data class MyData(@SerialName("DOI") val doi: String = "", @SerialName("val") val value: Int = 0)
-
-        val f = File("/home/ralf/IdeaProjects/LitBall-training/pred.json")
-        if (f.exists()) {
-            val lines = f.readLines()
-            val maps = lines.map {
-                json.decodeFromString<MyData>(it)
-            }
-            maps.forEach {
-                map[it.doi] = it.value
-            }
-        } else
-            throw Exception("File to open: ${CurrentPaperList.fileName} does not exist")
-
+    fun initialize() {
+        val jsonData = loadDataFromFile()
+        updateDataMap(jsonData)
         initialized = true
     }
-
     fun enrich(doi: String?): Int? {
-        return if (initialized)
-            map[doi]
-        else
-            null
+        return if (initialized) doiToValueMap[doi] else null
+    }
+
+    @Serializable
+    data class DataItem(@SerialName("DOI") val doi: String = "", @SerialName("val") val value: Int = 0)
+
+    private fun loadDataFromFile(): List<DataItem> {
+        val inputFile = File("/home/ralf/IdeaProjects/LitBall-training/pred.json")
+        require(inputFile.exists()) {
+            "File to open: ${CurrentPaperList.fileName} does not exist"
+        }
+
+        val lines = inputFile.readLines()
+        return lines.map { json.decodeFromString<DataItem>(it) }
+    }
+
+    private fun updateDataMap(dataList: List<DataItem>) {
+        dataList.forEach {
+            doiToValueMap[it.doi] = it.value
+        }
     }
 }
