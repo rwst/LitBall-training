4c5519687b6ae8b88e94257a7b577857de1d7336
Ralf Stephan <ralf@ark.in-berlin.de>
2023-08-02 15:53:26 +0200

    refactor

--- Tagger/src/main/kotlin/org/reactome/lit_ball_tagger/window/Rail.kt
+++ Tagger/src/main/kotlin/org/reactome/lit_ball_tagger/window/Rail.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.vector.ImageVector
 
 @Composable
 fun Rail(
@@ -19,61 +20,30 @@ fun Rail(
     onExit: () -> Unit,
 ) {
     val selectedItem by remember { mutableStateOf(0) }
-// TODO
-//    val items = listOf("Info", "Settings", "Exit")
-//    val icons = listOf(Icons.Filled.Settings, Icons.Filled.ExitToApp)
-//    val actions = listOf( {  } , { App.buttonSettings() }, { App.buttonExit() } )
+    data class RailItem(val text: String, val icon: ImageVector, val actionIndex: Int, val extraAction: (() -> Unit)? = null)
+
+    val items = listOf(
+        RailItem("Info", Icons.Filled.Info, 0),
+        RailItem("New", Icons.Filled.FileOpen, 1),
+        RailItem("Open", Icons.Filled.FileOpen, 2),
+        RailItem("Import", Icons.Filled.Download, 3),
+        RailItem("Export", Icons.Filled.Publish, 4),
+        RailItem("Save", Icons.Filled.Save, 5),
+        RailItem("Settings", Icons.Filled.Settings, 6),
+        RailItem("Exit", Icons.Filled.ExitToApp, 7, onExit)
+    )
+
     NavigationRail {
-        NavigationRailItem(
-            onClick = onRailItemClicked[0],
-            icon = { Icon(Icons.Filled.Info, null) },
-            label = { Text("Info") },
-            selected = selectedItem == 0
-        )
-        NavigationRailItem(
-            onClick = onRailItemClicked[1],
-            icon = { Icon(Icons.Filled.FileOpen, null) },
-            label = { Text("New") },
-            selected = selectedItem == 1
-        )
-        NavigationRailItem(
-            onClick = onRailItemClicked[2],
-            icon = { Icon(Icons.Filled.FileOpen, null) },
-            label = { Text("Open") },
-            selected = selectedItem == 2
-        )
-        NavigationRailItem(
-            onClick = onRailItemClicked[3],
-            icon = { Icon(Icons.Filled.Download, null) },
-            label = { Text("Import") },
-            selected = selectedItem == 3
-        )
-        NavigationRailItem(
-            onClick = onRailItemClicked[4],
-            icon = { Icon(Icons.Filled.Publish, null) },
-            label = { Text("Export") },
-            selected = selectedItem == 4
-        )
-        NavigationRailItem(
-            onClick = onRailItemClicked[5],
-            icon = { Icon(Icons.Filled.Save, null) },
-            label = { Text("Save") },
-            selected = selectedItem == 5
-        )
-        NavigationRailItem(
-            onClick = onRailItemClicked[6],
-            icon = { Icon(Icons.Filled.Settings, null) },
-            label = { Text("Settings") },
-            selected = selectedItem == 6
-        )
-        NavigationRailItem(
-            onClick = {
-                onRailItemClicked[7]()
-                onExit()
-            },
-            icon = { Icon(Icons.Filled.ExitToApp, null) },
-            label = { Text("Exit") },
-            selected = selectedItem == 7
-        )
+        items.forEach { item ->
+            NavigationRailItem(
+                onClick = {
+                    onRailItemClicked[item.actionIndex]()
+                    item.extraAction?.invoke()
+                },
+                icon = { Icon(item.icon, null) },
+                label = { Text(item.text) },
+                selected = selectedItem == item.actionIndex
+            )
+        }
     }
 }
